e97f2a37306b0fd32365c4550c1a035f54f54819
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-06-14 17:40:24 -0300

    starting feature 03

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Config from './pages/Config';
 import './App.css';
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <>
       <Route exact path="/" component={ Login } />
       <Route path="/game" component={ Game } />
+      <Route path="/config" component={ Config } />
     </>
   );
 }
---
+++ src/pages/Config.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Config extends React.Component {
+  render() {
+    return (
+      <h1>Configurações</h1>
+    );
+  }
+}
+
+export default Config;
