40f626013c688becdbf29b7043574ad5da99f3dc
denis-rossati <denis.rossati@hotmail.com>
2021-06-14 17:33:50 -0300

    try-3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import store from './store';
-import Login from './pages/Login';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={ store }>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-      </Switch>
+      <App />
     </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__(): f => f;
+
 const store = createStore(
   rootReducer, compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    devTools,
   ),
 );
 
