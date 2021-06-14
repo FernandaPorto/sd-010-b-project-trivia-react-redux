8f953a50b6529e58b944e7768e7ba86eddb9c4ff
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-06-14 17:11:15 -0300

    Feito requisito 1

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
 
 export default class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      active: true,
+      email: '',
+      nome: '',
+    };
+  }
+
+  validateEmail(mail) {
+    const format = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    // Alessandra passou o Regex que ela utilizou no projeto dela.
+    if (mail.match(format)) {
+      return true;
+    }
+    return false;
+  }
+
+  validateEmailAndPassword() {
+    const { email, nome } = this.state;
+    if (this.validateEmail(email) === true && nome) {
+      this.setState({
+        active: false,
+      });
+    } else {
+      this.setState({
+        active: true,
+      });
+    }
+  }
+
+  handleChange({ target: { id, value } }) {
+    this.setState({ [id]: value }, () => this.validateEmailAndPassword());
+  }
+
   render() {
+    const { active, nome, email } = this.state;
     return (
       <form>
-        <label htmlFor="name">
+        <label htmlFor="nome">
           Nome
-          <input type="type" id="name" data-testid="input-player-name" />
+          <input
+            type="type"
+            id="nome"
+            value={ nome }
+            onChange={ (e) => this.handleChange(e) }
+            data-testid="input-player-name"
+          />
         </label>
         <label htmlFor="email">
           Email
-          <input type="email" id="email" data-testid="input-gravatar-email" />
+          <input
+            type="email"
+            id="email"
+            value={ email }
+            onChange={ (e) => this.handleChange(e) }
+            data-testid="input-gravatar-email"
+          />
         </label>
-        <button type="submit" data-testid="btn-play">
+        <button type="submit" data-testid="btn-play" disabled={ active }>
           Jogar
         </button>
       </form>
