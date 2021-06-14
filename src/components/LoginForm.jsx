0942ac8b92b79ffb16a8ddc5ed3a83a0f5af1b3b
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-06-14 18:38:35 -0300

    Co-authored-by: Angelo Bittencourt <angelogpbl.info@gmail.com> Co-authored-by: MarceloPelegrino <MarceloPelegrino@users.noreply.github.com> Co-authored-by: Aline Barbosa <alinemelry@users.noreply.github.com>

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onHandleChange = this.onHandleChange.bind(this);
+    this.validateLogin = this.validateLogin.bind(this);
+
+    this.state = {
+      name: '',
+      email: '',
+    };
+  }
+
+  onHandleChange({ target }) {
+    this.setState({
+      [target.id]: target.value,
+    });
+  }
+
+  validateLogin() {
+    const { name, email } = this.state;
+
+    if (name.length > 6) {
+      return false;
+    }
+    return true;
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>Trivia</h1>
+        <form>
+          <label htmlFor="name">
+            Nome
+            <input
+              data-testid="input-player-name"
+              type="text"
+              id="name"
+              onChange={ this.onHandleChange }
+            />
+          </label>
+          <label htmlFor="email">
+            Email
+            <input
+              data-testid="input-gravatar-email"
+              type="email"
+              id="email"
+              onChange={ this.onHandleChange }
+            />
+          </label>
+          <input
+            data-testid="btn-play"
+            type="button"
+            value="Jogar"
+            disabled={ this.validateLogin() }
+          />
+        </form>
+      </main>
+    );
+  }
+}
+
+export default LoginForm;
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import userLogin from './userLogin';
+
+const rootReducers = combineReducers({ userLogin });
+
+export default rootReducers;
