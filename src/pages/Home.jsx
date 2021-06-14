28e00de8f3efd84d7aa73f9bf2264bd1a7724d92
Alan Tanaka <alanctnk@gmail.com>
2021-06-14 17:24:32 -0300

    Requisito 01 ok

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Login from '../components/Login';
+
+class Home extends Component {
+  render() {
+    return (
+      <Login />
+    );
+  }
+}
+
+export default Home;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  user,
+
 });
 
 export default rootReducer;
