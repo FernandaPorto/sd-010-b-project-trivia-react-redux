18f3895298355898c6a89818a01e904f72211015
denis-rossati <denis.rossati@hotmail.com>
2021-06-14 17:07:44 -0300

    try 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import Login from './pages/Login';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={ store }>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+const reducer = () => {
+  return {};
+}
+
+const rootReducer = combineReducers({ reducer });
+
+export default rootReducer;
