6e2ef4712e827303d65a2ef65a1e735c0d01228b
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-06-14 17:30:01 -0300

    feature 02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router';
 import Login from './pages/Login';
+import Game from './pages/Game';
 import './App.css';
 
 export default function App() {
   return (
-    <Route exact path="/" component={ Login } />
+    <>
+      <Route exact path="/" component={ Login } />
+      <Route path="/game" component={ Game } />
+    </>
   );
 }
