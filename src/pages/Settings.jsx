8f56f35b27582a276f979c400b90e2e25fa59e5f
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-06-14 19:51:03 -0300

    cria botao de voltar para home

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+/* import { Link } from 'react-router-dom'; */
 
 class Settings extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      redirect: false,
+    };
+
+    this.redirectHome = this.redirectHome.bind(this);
+  }
+
+  redirectHome() {
+    this.setState({
+      redirect: true,
+    });
+  }
+
   render() {
+    const { redirect } = this.state;
+
+    if (redirect) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <div>
         <h1 data-testid="settings-title">Configurações</h1>
-        <Link to="/">Voltar</Link>
+        <button
+          type="button"
+          onClick={ this.redirectHome }
+        >
+          Voltar
+        </button>
       </div>
     );
   }
