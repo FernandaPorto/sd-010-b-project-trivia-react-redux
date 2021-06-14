dce1b8978a478b247a4de82b8c1c6509c96e8644
Vinicius <vini_bodra@hotmail.com>
2021-06-14 18:17:30 -0300

    Requisito 1 - inicio

---
+++ src/pages/Login.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      nome: '',
+      disabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.verifyEmailAndName = this.verifyEmailAndName.bind(this);
+  }
+
+  handleChange(event) {
+    const { target: { id: key, value } } = event;
+    this.setState({
+      [key]: value,
+    },
+    this.verifyEmailAndName());
+  }
+
+  verifyEmailAndName() {
+    const { email, nome } = this.state;
+    if (email.length && nome.length) {
+      this.setState({
+        disabled: false,
+      });
+    }
+  }
+
+  render() {
+    const { email, nome, disabled } = this.state;
+    return (
+      <main>
+        <form>
+          <input
+            data-testid="email-input"
+            type="email"
+            id="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <input
+            data-testid="nome-input"
+            type="text"
+            id="nome"
+            value={ nome }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            id="button"
+            disabled={ disabled }
+          >
+            Jogar
+          </button>
+        </form>
+      </main>
+    );
+  }
+}
+
+export default Login;
