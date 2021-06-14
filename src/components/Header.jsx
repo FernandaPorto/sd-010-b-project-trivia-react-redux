85fbfa7ed9d8870b56bba0ef37a66a01a30e221c
RafaMaths <rmathscoach@gmail.com>
2021-06-14 17:43:30 -0300

    Co-authored-by: joao966 <joao966@users.noreply.github.com> Co-authored-by: Roger Franco <Roger-Franco@users.noreply.github.com>

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>nome</h1>
+          <img />
+          <p>placar</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
