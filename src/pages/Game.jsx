ddddd10b70f55e6d536ecac1012ef717fe664191
lucasportella <lucasportellaagu@gmail.com>
2021-06-15 14:04:32 -0300

    arruma aleatoriedade das respostas

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -29,10 +29,11 @@ class Game extends React.Component {
     const { apiResult } = this.props;
 
     if (apiResult.response_code === 0) {
-      const NUMERO_PARA_SORTEAR_RESPOSTAS = 5.0;
+      const NUMERO_PARA_SORTEAR_RESPOSTAS = 0.5;
       const answersArray = apiResult.results[indexQuestion].incorrect_answers
         .concat(apiResult.results[indexQuestion].correct_answer);
-      answersArray.sort(() => Math.random() - NUMERO_PARA_SORTEAR_RESPOSTAS);
+      const newAnswersArray = answersArray.sort(() => Math
+        .random() - NUMERO_PARA_SORTEAR_RESPOSTAS);
       return (
         <section>
           <p
@@ -42,7 +43,7 @@ class Game extends React.Component {
           </p>
           <p data-testid="question-text">{ apiResult.results[indexQuestion].question }</p>
           <section className="section-answer-buttons">
-            { answersArray.map((answer, index) => (
+            { newAnswersArray.map((answer, index) => (
               <button
                 data-testid={ answer === apiResult.results[indexQuestion].correct_answer
                   ? 'correct-answer' : `wrong-answer-${index}` }
