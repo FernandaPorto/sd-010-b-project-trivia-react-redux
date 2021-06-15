19304d55a72f747b484cfc95022a5eaf671b3177
ZeDasMelancia <78617230+AndersonPedrosa35@users.noreply.github.com>
2021-06-15 14:02:51 -0300

    Update suggestion correct App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import Login from './pages/Login';
-import React from 'react'
 
+import './App.css';
+
+import Login from './pages/Login';
 import Game from './pages/Game';
 import Feedback from './pages/Feedback';
 import Ranking from './pages/Ranking';
