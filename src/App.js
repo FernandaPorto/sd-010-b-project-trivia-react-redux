bc4d1fc0a3af1530194944ae655f8cd63f9d0e1f
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-06-15 14:12:40 -0300

    Arrumando lint

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
+
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Feedback from './pages/Feedback';
 import Ranking from './pages/Ranking';
 import Settings from './pages/Settings';
 
-export default function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route path="/setting" component={ Settings } />
-        <Route path="/feedback" component={ Feedback } />
-        <Route path="/ranking" component={ Ranking } />
-        <Route path="/game" component={ Game } />
-        <Route path="/" component={ Login } />
-      </Switch>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route path="/setting" component={ Settings } />
+          <Route path="/feedback" component={ Feedback } />
+          <Route path="/ranking" component={ Ranking } />
+          <Route path="/game" component={ Game } />
+          <Route path="/" component={ Login } />
+        </Switch>
+      </div>
+    );
+  }
 }
