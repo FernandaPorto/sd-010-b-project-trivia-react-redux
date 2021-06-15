b0a197548e57142b47bcce5e2575a3ab494d0531
Matheus H. Gois <matheus.hengo@gmail.com>
2021-06-15 16:39:20 -0300

    Feedback Header and info created

---
+++ src/pages/GameFeedback.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import GameHeader from '../components/GameHeader';
+
+class GameFeedback extends Component {
+  render() {
+    const { hits, score } = this.props;
+    const MIN_HITS = 3;
+
+    return (
+      <div>
+        <GameHeader />
+        <section>
+          <p data-testid="feedback-text">
+            {
+              hits < MIN_HITS
+                ? 'Podia ser melhor...'
+                : 'Mandou bem!'
+            }
+          </p>
+          <section>
+            <h1>Pontuação</h1>
+            <p data-testid="feedback-total-score">
+              { score }
+            </p>
+          </section>
+          <section>
+            <h1>Número de acertos</h1>
+            <p data-testid="feedback-total-question">
+              { hits }
+            </p>
+          </section>
+        </section>
+      </div>
+    );
+  }
+}
+
+// state.user.triviaGame
+const mapStateToProps = ({ user: { triviaGame: { score, hits } } }) => ({
+  userScore: score,
+  userHits: hits,
+});
+
+export default connect(mapStateToProps)(GameFeedback);
