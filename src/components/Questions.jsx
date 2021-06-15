a99897ece6090d769d5b9f0c7e5dfe6a76e7c470
Thiago Leite <leite.tjs@gmail.com>
2021-06-15 14:39:54 -0300

    requisito 5 e 6

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,44 +1,47 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Questions extends React.Component {
   render() {
-    // const { result: { category, question, correct_answer, incorrect_answers } } = this.props;
+    const { result:
+       { category,
+         question,
+         correct_answer: correctAnswer,
+         incorrect_answers: incorrectAnswers,
+       },
+    } = this.props;
     return (
-      <div>...</div>
+      <>
+        <h2 data-testid="question-category">
+          {category}
+        </h2>
+        <div data-testid="question-text">
+          {question}
+        </div>
+        <div data-testid="correct-answer">
+          {correctAnswer}
+        </div>
+        <div data-testid={ `wrong-answer${0}` }>
+          {incorrectAnswers[0]}
+        </div>
+        <div data-testid={ `wrong-answer${1}` }>
+          {incorrectAnswers[1]}
+        </div>
+        <div data-testid={ `wrong-answer${2}` }>
+          {incorrectAnswers[2]}
+        </div>
+      </>
     );
-
-    //   <>
-    //     <div data-testid="question-category">
-    //       {category}
-    //     </div>
-    //     <div data-testid="question-text">
-    //       {question}
-    //     </div>
-    //     <div data-testid="correct-answer">
-    //       {correct_answer}
-    //     </div>
-    //     <div>
-    //       {incorrect_answers[0]}
-    //     </div>
-    //     <div>
-    //       {incorrect_answers[1]}
-    //     </div>
-    //     <div>
-    //       {incorrect_answers[2]}
-    //     </div>
-    //   </>
-    // );
   }
 }
-// Questions.propTypes = {
-//   result: PropTypes.shape({
-//     category: PropTypes.string,
-//   }),
-// }.isRequired;
+Questions.propTypes = {
+  result: PropTypes.shape({
+    category: PropTypes.string,
+  }),
+}.isRequired;
 
-// Questions.default = {
-//   result: undefined,
-// };
+Questions.default = {
+  result: undefined,
+};
 
 export default Questions;
