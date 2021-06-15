84723a8c693efae55e74dbe799b82f91ad40e27d
lucasportella <lucasportellaagu@gmail.com>
2021-06-14 23:10:54 -0300

    uncomment test

--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -41,69 +41,69 @@ const BUTTON_PLAY_AGAIN_SELECTOR = '[data-testid="btn-play-again"]';
 
 // login
 
-// describe('1 - [TELA DE LOGIN] Crie a tela de login, onde a pessoa que joga deve preencher as informações para iniciar um jogo', () => {
-//   beforeEach(() => {
-//     cy.visit('http://localhost:3000/');
-//   });
+describe('1 - [TELA DE LOGIN] Crie a tela de login, onde a pessoa que joga deve preencher as informações para iniciar um jogo', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
 
-//   it('Escreve o nome da pessoa jogadora', () => {
-//     cy.get(INPUT_PLAYER_NAME_SELECTOR).type('Nome da pessoa');
-//   });
+  it('Escreve o nome da pessoa jogadora', () => {
+    cy.get(INPUT_PLAYER_NAME_SELECTOR).type('Nome da pessoa');
+  });
 
-//   it('Escreve o email da pessoa jogadora', () => {
-//     cy.get(INPUT_PLAYER_EMAIL_SELECTOR).type('email@pessoa.com');
-//   });
+  it('Escreve o email da pessoa jogadora', () => {
+    cy.get(INPUT_PLAYER_EMAIL_SELECTOR).type('email@pessoa.com');
+  });
 
-//   it('Botão Jogar desabilitado quando pessoa jogadora não preencher nenhum campo', () => {
-//     cy.get(BUTTON_PLAY_SELECTOR).should('be.disabled');
-//   });
+  it('Botão Jogar desabilitado quando pessoa jogadora não preencher nenhum campo', () => {
+    cy.get(BUTTON_PLAY_SELECTOR).should('be.disabled');
+  });
 
-//   it('Botão Jogar desabilitado quando pessoa jogadora escrever apenas o nome', () => {
-//     cy.get(INPUT_PLAYER_NAME_SELECTOR).type('Nome da pessoa');
-//     cy.get(BUTTON_PLAY_SELECTOR).should('be.disabled');
-//   });
+  it('Botão Jogar desabilitado quando pessoa jogadora escrever apenas o nome', () => {
+    cy.get(INPUT_PLAYER_NAME_SELECTOR).type('Nome da pessoa');
+    cy.get(BUTTON_PLAY_SELECTOR).should('be.disabled');
+  });
 
-//   it('Botão Jogar desabilitado quando pessoa jogadora escrever apenas o email', () => {
-//     cy.get(INPUT_PLAYER_EMAIL_SELECTOR).type('email@pessoa.com');
-//     cy.get(BUTTON_PLAY_SELECTOR).should('be.disabled');
-//   });
+  it('Botão Jogar desabilitado quando pessoa jogadora escrever apenas o email', () => {
+    cy.get(INPUT_PLAYER_EMAIL_SELECTOR).type('email@pessoa.com');
+    cy.get(BUTTON_PLAY_SELECTOR).should('be.disabled');
+  });
 
-//   it('Botão Jogar habilitado quando pessoa jogadora preencher os campos de nome e email', () => {
-//     cy.get(INPUT_PLAYER_NAME_SELECTOR).type('Nome da pessoa');
-//     cy.get(INPUT_PLAYER_EMAIL_SELECTOR).type('email@pessoa.com');
-//     cy.get(BUTTON_PLAY_SELECTOR).should('not.be.disabled');
-//   });
-// });
+  it('Botão Jogar habilitado quando pessoa jogadora preencher os campos de nome e email', () => {
+    cy.get(INPUT_PLAYER_NAME_SELECTOR).type('Nome da pessoa');
+    cy.get(INPUT_PLAYER_EMAIL_SELECTOR).type('email@pessoa.com');
+    cy.get(BUTTON_PLAY_SELECTOR).should('not.be.disabled');
+  });
+});
 
-// describe('2 - [TELA DE LOGIN] Crie o botão de iniciar o jogo', () => {
-//   beforeEach(() => {
-//     cy.visit('http://localhost:3000/');
-//     cy.clearLocalStorage();
-//   });
+describe('2 - [TELA DE LOGIN] Crie o botão de iniciar o jogo', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+    cy.clearLocalStorage();
+  });
 
-//   it('Inicia jogo salvando um token de jogador', () => {
-//     cy.get(INPUT_PLAYER_NAME_SELECTOR).type('Nome da pessoa');
-//     cy.get(INPUT_PLAYER_EMAIL_SELECTOR).type('email@pessoa.com');
-//     cy.get(BUTTON_PLAY_SELECTOR).click().should(() => {
-//       expect(localStorage.getItem(TOKEN_KEY)).not.to.be.null;
-//     });
-//   });
-// });
+  it('Inicia jogo salvando um token de jogador', () => {
+    cy.get(INPUT_PLAYER_NAME_SELECTOR).type('Nome da pessoa');
+    cy.get(INPUT_PLAYER_EMAIL_SELECTOR).type('email@pessoa.com');
+    cy.get(BUTTON_PLAY_SELECTOR).click().should(() => {
+      expect(localStorage.getItem(TOKEN_KEY)).not.to.be.null;
+    });
+  });
+});
 
-// describe('3 - [TELA DE LOGIN] Crie um botão na tela inicial que leve para a tela de configurações', () => {
-//   beforeEach(() => {
-//     cy.visit('http://localhost:3000/');
-//   });
+describe('3 - [TELA DE LOGIN] Crie um botão na tela inicial que leve para a tela de configurações', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
 
-//   it('O botão deve existir na página', () => {
-//     cy.get(BUTTON_SETTINGS_SELECTOR).should('exist');
-//   });
+  it('O botão deve existir na página', () => {
+    cy.get(BUTTON_SETTINGS_SELECTOR).should('exist');
+  });
 
-//   it('A tela de configurações deve possuir um título', () => {
-//     cy.get(BUTTON_SETTINGS_SELECTOR).click();
-//     cy.get(SETTINGS_TITLE_SELECTOR).should('exist');
-//   });
-// });
+  it('A tela de configurações deve possuir um título', () => {
+    cy.get(BUTTON_SETTINGS_SELECTOR).click();
+    cy.get(SETTINGS_TITLE_SELECTOR).should('exist');
+  });
+});
 
 // game
 
