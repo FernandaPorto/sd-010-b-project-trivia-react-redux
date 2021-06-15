21bf42eb17616cb3a86032bf5228686651f845a2
RafaMaths <rmathscoach@gmail.com>
2021-06-15 16:42:44 -0300

    Co-authored-by: joao966 <joao966@users.noreply.github.com> Co-authored-by: Roger Franco <Roger-Franco@users.noreply.github.com>

--- src/pages/Trivia.js
+++ src/pages/Trivia.js
@@ -2,38 +2,104 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
-import { fetchApiQuestions } from '../actions/index';
+import { fetchApiQuestions, fetchAPI } from '../actions/index';
 
 class Trivia extends React.Component {
   constructor() {
     super();
     this.state = {
       questions: [],
+      // answers: [],
     };
+    this.renderQuestion = this.renderQuestion.bind(this);
+    // this.funcao = this.funcao.bind(this);
+  }
+
+  componentDidMount() {
+    this.getQuestions();
   }
 
   async getQuestions() {
+    const { thunkToken } = this.props;
     const quantityQuestions = 5;
-    const { questions } = this.state;
-    const token = localStorage.getItem('token');
-    console.log(token);
+    const token = await thunkToken();
+    const tokenRequisition = token.result.token;
     const { apiQuestions } = this.props;
-    const response = await apiQuestions(token, quantityQuestions);
+    const response = await apiQuestions(tokenRequisition, quantityQuestions);
     this.setState({
       questions: response.questions.results,
     });
+    const { questions } = this.state;
     console.log(questions);
   }
 
+  // answer === correct ? 'correct-answer' : 'wrong-answer-${index}'
+
+  renderQuestion() {
+    const { questions } = this.state;
+    return (
+      questions.map((question, index) => (
+        <ul key={ question.category }>
+          <p data-testid="question-category">{question.category}</p>
+          <li data-testid="question-text">
+            {question.question}
+            {' '}
+            <button type="button">
+              <p
+                data-testid="correct-answer"
+              >
+                {question.correct_answer}
+              </p>
+            </button>
+            {question.incorrect_answers.map((incorrect) => (
+              <button key={ incorrect } type="button">
+                <p
+                  data-testid={ `wrong-answer-${index}` }
+                >
+                  {incorrect}
+                </p>
+              </button>
+            ))}
+          </li>
+        </ul>
+      )));
+  }
+
+  // colorAnswers() {
+  //   const { correct_answer: correct, answer } = this.state;
+  //   return answers.map((answer, index) => {
+  //     const seeColor = answer === correct ? '3px solid rgb(6, 240, 15)' : '3px solid rgb(255, 0, 0)';
+
+  //     const verifyColor = answer === correct ? 'correct-answer' : `wrong-answer-${index}`;
+  //     return (
+  //       <button>
+  //
+
+  //     );
+  //   });
+  // }
+
+  // funcao() {
+  //   const { questions } = this.state;
+  //   const questao = questions[0];
+  //   return questao;
+  //   // console.log(questao);
+  // }
+
   render() {
+    // const { questions } = this.state;
+    // const questao = questions[0].question
+    // console.log(this.funcao());
     return (
       <div>
         <Header />
-        <h2 data-testid="question-category">questão</h2>
-        <p data-testid="question-text" />
+        {/* <p data-testid="question-category">{questions.category}</p>
+        <p data-testid="question-text">{questions.question}</p>
+        <p>{questions.}</p> */}
+        {this.renderQuestion()}
         {/* <button
           type="button"
-          data-testid={ questions === correctAnswer ? 'correct-answer' : `wrong-answer${index}` }
+          -testid={ questions === correctAnswer ? 'correct-answer' : `wrong-answer${index}` }
         /> */}
       </div>
     );
@@ -44,12 +110,13 @@ Trivia.propTypes = {
   apiQuestions: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  questions: state.reducerQuestion.questions,
-});
+// const mapStateToProps = (state) => ({
+//   questions: state.reducerQuestion.questions,
+// });
 
 const mapDispatchToProps = (dispatch) => ({
   apiQuestions: (token, perguntas) => dispatch(fetchApiQuestions(token, perguntas)),
+  thunkToken: () => dispatch(fetchAPI()),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Trivia);
+export default connect(null, mapDispatchToProps)(Trivia);
