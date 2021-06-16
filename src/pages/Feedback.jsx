48e990e207d39ada274698a8e804609b03048967
lucasportella <lucasportellaagu@gmail.com>
2021-06-16 11:30:41 -0300

    req 1-14 passando, req 15 inicio

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import FeedbackHeader from './FeedbackHeader';
 
 class Feedback extends React.Component {
+  // componentDidMount() {
+  //   const estadoInicial = {
+  //     player: {
+  //       name: '',
+  //       assertions: 0,
+  //       score: 0,
+  //       gravatarEmail: '',
+  //     },
+  //   };
+  //   const estadoInicialJson = JSON.stringify(estadoInicial);
+  //   localStorage.setItem('state', estadoInicialJson);
+
+  //   const token = '';
+  //   localStorage.setItem('token', token);
+  // }
+
   render() {
     const {
-      playerReducer: { assertions },
+      playerReducer: { assertions, score },
     } = this.props;
     const goodScore = 3;
     return (
@@ -17,6 +34,11 @@ class Feedback extends React.Component {
         ) : (
           <h1 data-testid="feedback-text">Podia ser melhor...</h1>
         )}
+        <h2 data-testid="feedback-total-score">{score}</h2>
+        <h2 data-testid="feedback-total-question">{assertions}</h2>
+        <Link to="/">
+          <button type="button">Jogar novamente</button>
+        </Link>
       </main>
     );
   }
@@ -31,5 +53,6 @@ export default connect(mapStateToProps)(Feedback);
 Feedback.propTypes = {
   playerReducer: PropTypes.shape({
     assertions: PropTypes.number.isRequired,
+    score: PropTypes.number.isRequired,
   }).isRequired,
 };
