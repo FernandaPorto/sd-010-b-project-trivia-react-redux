adf8db8e03316170bffbbca4ec818885b8e34090
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-06-16 14:31:20 -0300

    mudanças no codigo do req7

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,18 +3,52 @@ import PropTypes from 'prop-types';
 // comentario p add
 
 class Question extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      correctButton: false,
+      wrongButton: false,
+    };
+    this.handleButtonColor = this.handleButtonColor.bind(this);
+    this.handleResult = this.handleResult.bind(this);
+    this.insertDataTestId = this.insertDataTestId.bind(this);
+    this.insertClass = this.insertClass.bind(this);
+  }
+
+  handleButtonColor() {
+    this.setState({
+      correctButton: true,
+      wrongButton: true,
+    });
+  }
+
+  handleResult() {
     const { result } = this.props;
     const arrAnswers = [result.correct_answer, ...result.incorrect_answers];
     const half = 0.5;
     const shuffleArray = (array) => array.sort(() => Math.random() - half);
-    const arrRandom = shuffleArray(arrAnswers);
-    const check = (answer, index) => {
-      if (answer === result.correct_answer) {
-        return 'correct-answer';
-      }
-      return `wrong-answer-${index}`;
-    };
+    return shuffleArray(arrAnswers);
+  }
+
+  insertDataTestId(answer, index) {
+    const { result } = this.props;
+    if (answer === result.correct_answer) {
+      return 'correct-answer';
+    }
+    return `wrong-answer-${index}`;
+  }
+
+  insertClass(answer) {
+    const { result } = this.props;
+    if (answer === result.correct_answer) {
+      return 'correctButton';
+    }
+    return 'wrongButton';
+  }
+
+  render() {
+    const { result } = this.props;
+    const arrRandom = this.handleResult();
     return (
       <>
         <span data-testid="question-category">
@@ -35,7 +69,9 @@ class Question extends React.Component {
           <button
             type="button"
             key={ answer }
-            data-testid={ check(answer, index) }
+            data-testid={ this.insertDataTestId(answer, index) }
+            className={ this.insertClass(answer) }
+            onClick={ this.handleButtonColor }
           >
             { answer }
           </button>
