1ab168c26b44e2bab049534f161306a221a1a3c2
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-16 00:01:22 -0300

    removendo fragment na importação do react

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
 import Question from '../components/Question';
