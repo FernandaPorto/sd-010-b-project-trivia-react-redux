266b32c89e88c7eb5594358ff498a83685ba3c50
RafaMaths <rmathscoach@gmail.com>
2021-06-16 15:36:24 -0300

    Co-authored-by: Roger Franco <Roger-Franco@users.noreply.github.com> Co-authored-by: joao966 <joao966@users.noreply.github.com>

--- src/pages/Trivia.js
+++ src/pages/Trivia.js
@@ -13,13 +13,34 @@ class Trivia extends React.Component {
       questions: [],
       correct: '',
       reject: '',
+      seconds: 30,
+      disable: false,
     };
     this.renderQuestion = this.renderQuestion.bind(this);
     this.colorAnswers = this.colorAnswers.bind(this);
+    this.update = this.update.bind(this);
   }
 
   componentDidMount() {
     this.getQuestions();
+    const ONE_SECOND = 1000; // 1 second in miliseconds
+    const { seconds } = this.state;
+    this.cronometerInterval = setInterval(() => {
+      console.log('interval rodando');
+      this.setState((state) => {
+        if (seconds) {
+          return { seconds: state.seconds - 1 };
+        }
+      });
+    }, ONE_SECOND);
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    this.update(prevState);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.cronometerInterval);
   }
 
   async getQuestions() {
@@ -34,20 +55,29 @@ class Trivia extends React.Component {
     });
   }
 
+  update(state) {
+    const MIN_SECONDS = 0;
+    if (state.seconds === MIN_SECONDS) {
+      this.setState({ seconds: 30, correct: 'correct_answer', reject: 'incorrect_answer', disable: true });
+    }
+  }
+
   colorAnswers() {
     this.setState({ correct: 'correct_answer', reject: 'incorrect_answer' });
   }
 
   renderQuestion() {
-    const { questions, correct, reject } = this.state;
+    const { questions, correct, reject, disable } = this.state;
     return (
       questions.map((question, index) => (
+
         <ul key={ question.category }>
           <p data-testid="question-category">{question.category}</p>
           <li data-testid="question-text">
             {question.question}
             {' '}
             <button
+              disabled={ disable }
               className={ correct }
               data-testid="correct-answer"
               onClick={ this.colorAnswers }
@@ -59,6 +89,7 @@ class Trivia extends React.Component {
             </button>
             {question.incorrect_answers.map((incorrect) => (
               <button
+                disabled={ disable }
                 data-testid={ `wrong-answer-${index}` }
                 onClick={ this.colorAnswers }
                 key={ incorrect }
@@ -76,9 +107,13 @@ class Trivia extends React.Component {
   }
 
   render() {
+    const { seconds } = this.state;
     return (
       <div>
         <Header />
+        <h3>
+          {seconds}
+        </h3>
         {this.renderQuestion()}
       </div>
     );
@@ -97,3 +132,5 @@ const mapDispatchToProps = (dispatch) => ({
 
 export default connect(null, mapDispatchToProps)(Trivia);
 // errei o nome do commit
+
+/* para criação do cronômetro, utilizamos como referência o exercício do bloco 13.1 feito pelo instrutor Ícaro <https://github1s.com/tryber/sd-10b-live-lectures/tree/lecture/13.1> */
