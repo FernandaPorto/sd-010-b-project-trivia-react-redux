f804de52e2e9e30a3c33b22de6ffd48ba1869165
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-06-16 16:37:48 -0300

    added Route Feedback in App.js

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class Feedback extends Component {
   render() {
     return (
       // <HeaderFeedback /> aqui sera importado o header de feedback
-      <div />
+      <div>
+        <button type="button">
+          <Link to="/">Jogar novamente</Link>
+        </button>
+      </div>
     );
   }
 }
