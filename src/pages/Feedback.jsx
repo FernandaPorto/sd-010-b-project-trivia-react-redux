723a01325bbb17826a56eaa18fedbb2cacf8b60a
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-06-16 16:31:59 -0300

    Feat: Requirement 15 -Starting requirement

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Feedback extends Component {
+  render() {
+    return (
+      // <HeaderFeedback /> aqui sera importado o header de feedback
+      <div />
+    );
+  }
+}
+
+export default Feedback;
