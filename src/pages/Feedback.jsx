e4b0ebbab0a6ff33748733e96e69718c6b49c66a
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-06-16 16:56:08 -0300

    Feat: Requirement 15 -DONE - Changes made, placing requirement code 14"

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,18 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import HeaderFeedback from '../components/HeaderFeedback';
 
-class Feedback extends Component {
+class Feedback extends React.Component {
   render() {
+    const { assertions, score } = this.props;
     return (
-      // <HeaderFeedback /> aqui sera importado o header de feedback
-      <div>
+      <section>
+        <h1>Feedback</h1>
+        <HeaderFeedback />
+        <div>
+          <span>Número de acertos: </span>
+          <span data-testid="feedback-total-question">{assertions}</span>
+        </div>
+        <div>
+          <span>Pontuação final: </span>
+          <span data-testid="feedback-total-score">{score}</span>
+        </div>
         <button type="button" data-testid="btn-play-again">
           <Link to="/">Jogar novamente</Link>
         </button>
-      </div>
+      </section>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+  score: state.player.score,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+  score: PropTypes.number,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
