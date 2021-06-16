df9c76a6158af7dbd0ab7bcdd6d46db593c8474c
Aladino Borges <eualadino@gmail.com>
2021-06-16 09:24:47 -0300

    Imports TriviaGame.

--- src/components/TriviaGame.jsx
+++ src/components/TriviaGame.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
 class Trivia extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateState = this.updateState.bind(this);
+    this.renderQuestion = this.renderQuestion.bind(this);
+
+
+    this.state = {
+      loading: true,
+      questions: [],
+    };
+  }
+  
   render () {
     return ();
   }
