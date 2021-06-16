708929614b507f7c3f5b5862516a7135188b90ef
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-16 01:46:22 -0300

    adicionando o constructor e a função generateRandomAnswers

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,6 +3,28 @@ import { connect } from 'react-redux';
 import propTypes from 'prop-types';
 
 class Question extends Component {
+  constructor() {
+    super();
+    this.generateRandomAnswers = this.generateRandomAnswers.bind(this);
+  }
+
+  generateRandomAnswers(correctAnswer, incorrectAnswer) {
+    const range = 20;
+    const getRandom = () => Math.ceil(Math.random() * range);
+    if (inc.length < 2) {
+      return [
+        { id: getRandom(), answer: correctAnswer, dataTestId: 'correct-answer' },
+        { id: getRandom(), answer: incorrectAnswer, dataTestId: 'wrong-answer-0' },
+      ].sort((a, b) => +(a.id > b.id) || +(a.id === b.id) - 1); // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    }
+    return [
+      { id: getRandom(), answer: correctAnswer, dataTestId: 'correct-answer' },
+      { id: getRandom(), answer: incorrectAnswer[0], dataTestId: 'wrong-answer-0' },
+      { id: getRandom(), answer: incorrectAnswer[1], dataTestId: 'wrong-answer-1' },
+      { id: getRandom(), answer: incorrectAnswer[2], dataTestId: 'wrong-answer-2' },
+    ].sort((a, b) => +(a.id > b.id) || +(a.id === b.id) - 1); // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  }
+
   render() {
     const { questions, index } = this.props;
 
