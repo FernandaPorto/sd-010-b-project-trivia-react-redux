34742a5cc2b26b5986eac41ed6c3ef67993afb25
edersonrodara <edersonrodara@gmail.com>
2021-06-16 19:31:16 -0300

    Grupo 29 - Requisito 13 - concluido

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -5,21 +5,33 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  feedBack(assertions) {
+    const NUMBER_THREE = 3;
+    switch (true) {
+    case (assertions < NUMBER_THREE):
+      return <h2 data-testid="feedback-text">Podia ser melhor...</h2>;
+    case (assertions >= NUMBER_THREE):
+      return <h2 data-testid="feedback-text">Mandou bem!</h2>;
+    default:
+      return <h2>Error</h2>;
+    }
+  }
+
   render() {
     const { score, assertions } = this.props;
     return (
       <section>
         <Header />
-        <h2 data-testid="feedback-text">
-          Mensagem de Feedback
-        </h2>
+        { this.feedBack(assertions) }
+        <p>Acertos:</p>
         <p data-testid="feedback-total-question">
           {assertions}
           {' '}
         </p>
-        <p data-testid="feedback-total-score">{score}</p>
-        {console.log(assertions)}
-        {console.log(score)}
+        <p>Pontos:</p>
+        <p data-testid="feedback-total-score">
+          {score}
+        </p>
         <button type="button">VER RANKING</button>
         <Link to="/"><button type="button">JOGAR NOVAMENTE</button></Link>
 
