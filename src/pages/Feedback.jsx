3271d56de455adc26e3cf55ef00396c61282f725
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-06-16 17:54:38 -0300

    "Feat: Requirement 18 -DONE"

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import HeaderFeedback from '../components/HeaderFeedback';
+
+class Feedback extends React.Component {
+  render() {
+    const { assertions, score } = this.props;
+    return (
+      <section>
+        <h1>Feedback</h1>
+        <HeaderFeedback />
+        <div>
+          <span>Número de acertos: </span>
+          <span data-testid="feedback-total-question">{assertions}</span>
+        </div>
+        <div>
+          <span>Pontuação final: </span>
+          <span data-testid="feedback-total-score">{score}</span>
+        </div>
+        <button type="button" data-testid="btn-play-again">
+          <Link to="/game">Jogar novamente</Link>
+        </button>
+        <button type="button" data-testid="btn-ranking">
+          <Link to="/ranking">Ver Ranking</Link>
+        </button>
+        <button type="button" data-testid="btn-go-home">
+          <Link to="/">Voltar para login</Link>
+        </button>
+      </section>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+  score: state.player.score,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+  score: PropTypes.number,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
