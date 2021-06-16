4b9f0203b2e53bee96ed627af6068fff83a43d85
edersonrodara <edersonrodara@gmail.com>
2021-06-16 16:04:11 -0300

    Grupo 29 - Requesito 12 - concluído

--- src/pages/Configurations.jsx
+++ src/pages/Configurations.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Configurations extends Component {
   render() {
     return (
       <section>
         <h1 data-testid="settings-title">Configurações</h1>
+        <Link to="/"><button type="button">Voltar</button></Link>
       </section>
     );
   }
