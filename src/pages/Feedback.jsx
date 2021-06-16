9c6988d13caf0d9d5b8759d53d3af55e4b4a71f9
lucasportella <lucasportellaagu@gmail.com>
2021-06-16 10:40:13 -0300

    req 11 e 13 prontos

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+// import FeedbackHeader from './FeedbackHeader';
+
+class Feedback extends React.Component {
+  render() {
+    const {
+      playerReducer: { assertions },
+    } = this.props;
+    const goodScore = 3;
+    return (
+      <main>
+        {assertions >= goodScore ? (
+          <h1 data-testid="feedback-text">Mandou bem!</h1>
+        ) : (
+          <h1 data-testid="feedback-text">Podia ser melhor...</h1>
+        )}
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  playerReducer: state.player,
+});
+
+export default connect(mapStateToProps)(Feedback);
+
+Feedback.propTypes = {
+  playerReducer: PropTypes.shape({
+    assertions: PropTypes.number.isRequired,
+  }).isRequired,
+};
