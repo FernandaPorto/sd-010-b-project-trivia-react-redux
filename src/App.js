aa1ffed76a7e7db06d3874b40c1df83fb5acc1b3
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-06-16 17:10:49 -0300

    Feat: Requirement 16 -DONE -Created ranking route and direction button for ranking

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import store from './redux/store';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Game from './pages/Game';
+import Ranking from './pages/Ranking';
 // import logo from './trivia.png';
 import './App.css';
 
@@ -16,6 +17,7 @@ export default function App() {
           <Route exact path="/" component={ Login } />
           <Route path="/settings" component={ Settings } />
           <Route path="/game" component={ Game } />
+          <Route path="/ranking" component={ Ranking } />
         </Switch>
       </BrowserRouter>
     </Provider>
---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import HeaderFeedback from '../components/HeaderFeedback';
+
+class Feedback extends React.Component {
+  render() {
+    const { assertions, score } = this.props;
+    return (
+      <section>
+        <h1>Feedback</h1>
+        <HeaderFeedback />
+        <div>
+          <span>Número de acertos: </span>
+          <span data-testid="feedback-total-question">{assertions}</span>
+        </div>
+        <div>
+          <span>Pontuação final: </span>
+          <span data-testid="feedback-total-score">{score}</span>
+        </div>
+        <button type="button" data-testid="btn-play-again">
+          <Link to="/">Jogar novamente</Link>
+        </button>
+        <button type="button" data-testid="btn-ranking">
+          <Link to="/">Ver Ranking</Link>
+        </button>
+      </section>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+  score: state.player.score,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+  score: PropTypes.number,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
