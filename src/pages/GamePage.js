f072a7f687445e07ea927e9ebfaa15ece5598f83
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-16 00:02:43 -0300

    refatoração parágrafo para exibição da variável na mesma linha

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -24,9 +24,7 @@ class GamePage extends React.Component {
       <>
         <header>
           <img src="https://www.gravatar.com/avatar/" data-testid="header-profile-picture" alt="gravatar" />
-          <p data-testid="header-player-name">
-            { name }
-          </p>
+          <p data-testid="header-player-name">{ name }</p>
           <p data-testid="header-score">0</p>
         </header>
         <Question index={ index } />
