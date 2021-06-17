9be05fba1d609c20718c5812767464bcb058c178
Dieghonm <dieghonm@gmail.com>
2021-06-17 03:48:30 -0300

    Requisito 16, tentativa 3

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 class Ranking extends Component {
   render() {
     let getRanking = JSON.parse(localStorage.getItem('ranking'));
+    const atual = JSON.parse(localStorage.getItem('state'));
     if (getRanking === null) {
       getRanking = [];
     }
@@ -13,10 +14,16 @@ class Ranking extends Component {
       <section>
         <Header />
         <h1 data-testid="ranking-title">RANKING</h1>
+        <h3
+          data-testid="player-name-0"
+        >
+          {`Rodada atual - nome: ${atual.player.name} 
+        com ${atual.player.score} pontos`}
+        </h3>
         {getRanking.map((item, i) => (
           <h3
             key={ i }
-            data-testid={ `player-name-${i}` }
+            data-testid={ `player-name-${i + 1}` }
           >
             {`${item.name} - ${item.score} pontos`}
           </h3>))}
