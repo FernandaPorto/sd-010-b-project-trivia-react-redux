b0a64ddc3d70ed9a5944723937ba333f8b4530d7
Miguel Dantas <miiguelalb@gmail.com>
2021-06-17 16:33:21 -0300

    Requisitos 9 e 11 concluidos

--- src/components/GameAsks.js
+++ src/components/GameAsks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class GameAsks extends Component {
   constructor(props) {
@@ -9,15 +10,25 @@ class GameAsks extends Component {
       loading: true,
       asks: [],
       answer: false,
-      disabled: false,
+      countDown: 30,
+      disabledButton: false,
+      redirect: false,
     };
     this.answer = this.answer.bind(this);
+    this.counter = this.counter.bind(this);
     this.nextQuestion = this.nextQuestion.bind(this);
     this.setAsksState = this.setAsksState.bind(this);
+    this.timer = this.timer.bind(this);
+    this.totalSum = this.totalSum.bind(this);
   }
 
   componentDidMount() {
     this.setAsksState();
+    this.counter();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeInterval);
   }
 
   async setAsksState() {
@@ -32,25 +43,67 @@ class GameAsks extends Component {
     });
   }
 
-  // correctClick({ target }) {
-  //   target.style.border = '3px solid rgb(6, 240, 15)';
-  //   const element = document.querySelector('.wrong-answer');
-  //   element.disabled = true;
-  // }
+  counter() {
+    const SECOND = 1000;
+    this.timeInterval = setInterval(this.timer, SECOND);
+  }
 
-  // incorrectClick({ target }) {
-  //   target.style.border = '3px solid rgb(255, 0, 0)';
-  //   const element = document.querySelector('.correct-answer');
-  //   element.disabled = true;
-  // }
+  timer() {
+    const { countDown } = this.state;
+    if (countDown <= 0) {
+      this.setState({
+        countDown: 0,
+      });
+    } else {
+      this.setState((state) => ({
+        countDown: state.countDown - 1,
+      }));
+    }
+    if (countDown === 0) {
+      this.setState({
+        disabledButton: true,
+      });
+    }
+  }
 
-  nextQuestion({ target }) {
-    const { indexQuestion, asks, disabled } = this.state;
+  totalSum() {
+    const { asks, indexQuestion, countDown } = this.state;
+    const localStorageInfos = JSON.parse(localStorage.getItem('state'));
+    const { player: { score, assertions } } = localStorageInfos;
+    const UM = 1;
+    const DOIS = 2;
+    const TRES = 3;
+    const DEZ = 10;
+    switch (asks[indexQuestion].difficulty) {
+    case (asks[indexQuestion].difficulty === 'hard'):
+      localStorageInfos.player.score = score + DEZ + (countDown * TRES);
+      localStorageInfos.player.assertions = assertions + 1;
+      localStorage.setItem('state', JSON.stringify(localStorageInfos));
+      break;
+    case (asks[indexQuestion].difficulty === 'medium'):
+      localStorageInfos.player.score = score + DEZ + (countDown * DOIS);
+      localStorageInfos.player.assertions = assertions + 1;
+      localStorage.setItem('state', JSON.stringify(localStorageInfos));
+      break;
+    default:
+      localStorageInfos.player.score = score + DEZ + (countDown * UM);
+      localStorageInfos.player.assertions = assertions + 1;
+      localStorage.setItem('state', JSON.stringify(localStorageInfos));
+      break;
+    }
+  }
+
+  nextQuestion() {
+    clearInterval(this.counter);
+    const { indexQuestion, asks } = this.state;
     this.setState((state) => ({
       indexQuestion: state.indexQuestion + 1,
+      countDown: 30,
+      disabledButton: false,
+      answer: false,
     }));
-    if (asks.length - 2 === indexQuestion) {
-      this.setState({ disabled: true });
+    if (asks.length - 1 === indexQuestion) {
+      this.setState({ redirect: true });
     }
   }
 
@@ -61,21 +114,20 @@ class GameAsks extends Component {
   }
 
   render() {
-    const { indexQuestion, loading, asks, answer, disabled } = this.state;
-    if (loading) {
-      return (
-        <span>Carregando...</span>
-      );
-    }
-    console.log(disabled);
+    const { indexQuestion,
+      loading, asks, answer, countDown, disabledButton, redirect } = this.state;
+    if (loading) return <span>Carregando...</span>;
+    if (redirect) return <Redirect to="/feedback" />;
     return (
       <section>
+        <p>{countDown}</p>
         <p data-testid="question-category">{ asks[indexQuestion].category }</p>
         <h1 data-testid="question-text">{asks[indexQuestion].question}</h1>
         {asks[indexQuestion].incorrect_answers.map((element, indexI) => (
           <button
             key={ indexI }
             type="button"
+            disabled={ disabledButton }
             onClick={ this.answer }
             className={ answer ? 'incorrect' : 'null' }
             data-testid={ `wrong-answer-${indexI}` }
@@ -85,8 +137,12 @@ class GameAsks extends Component {
         ))}
         <button
           type="button"
+          disabled={ disabledButton }
           data-testid="correct-answer"
-          onClick={ this.answer }
+          onClick={ () => {
+            this.answer();
+            this.totalSum();
+          } }
           className={ answer ? 'correct' : 'null' }
         >
           {asks[indexQuestion].correct_answer}
@@ -94,11 +150,11 @@ class GameAsks extends Component {
         <br />
         <button
           type="button"
+          style={ { display: `${answer ? 'block' : 'none'}` } }
           data-testid="btn-next"
-          disabled={ disabled }
           onClick={ this.nextQuestion }
         >
-          Próxima Pergunta
+          Próxima
         </button>
       </section>
     );
