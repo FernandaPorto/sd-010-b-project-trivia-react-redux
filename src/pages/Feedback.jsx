56b39d7be8585c90f6754ffef4ebf5e12196b98d
denis-rossati <denis.rossati@hotmail.com>
2021-06-17 14:44:04 -0300

    finalizando requisito 16

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -4,13 +4,34 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class Feedback extends Component {
-  congratulatios() {
+  congratulations() {
     const { playerAssertions } = this.props;
     const minimumAssert = 3;
     if (playerAssertions < minimumAssert) {
       return ('Podia ser melhor...');
     }
     return ('Mandou bem!');
+  }
+
+  addToRanking() {
+    const { playerName, playerScore, playerImage } = this.props;
+    if (!localStorage.ranking) {
+      const player = {
+        name: playerName,
+        score: playerScore,
+        picture: playerImage,
+      };
+      localStorage.setItem('ranking', JSON.stringify([player]));
+    } else {
+      const player = {
+        name: playerName,
+        score: playerScore,
+        picture: playerImage,
+      };
+      const rankingList = JSON.parse(localStorage.getItem('ranking'));
+      const newRankingList = [...rankingList, player];
+      localStorage.setItem('ranking', JSON.stringify(newRankingList));
+    }
   }
 
   render() {
@@ -30,10 +51,17 @@ class Feedback extends Component {
             { playerScore }
           </h4>
         </header>
-        <h2 data-testid="feedback-text">{ this.congratulatios() }</h2>
+        <h2 data-testid="feedback-text">{ this.congratulations() }</h2>
         <h3 data-testid="feedback-total-score">{ playerScore }</h3>
         <h3 data-testid="feedback-total-question">{ playerAssertions }</h3>
         <Link to="/" data-testid="btn-play-again">Jogar novamente</Link>
+        <Link
+        to="/ranking"
+          data-testid="btn-ranking"
+          onClick={ this.addToRanking() }
+        >
+          Ver Ranking
+        </Link>
       </div>
     );
   }
---
+++ src/pages/Ranking.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Ranking extends Component {
+  render() {
+    return (
+      <div>
+        <h1 data-testid="ranking-title">RANKING</h1>
+        <Link to="/" data-testid="btn-go-home">Tente Novamente</Link>
+      </div>
+    )
+  }
+}
