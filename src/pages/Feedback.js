97e1688ead32595724543ca59347f4c1989fd3b4
Fernando <fhomaia@gmail.com>
2021-06-17 11:55:40 -0300

    Total no Header e erros de lint corrigidos

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
+import PropTypes from 'prop-types';
 
 class Feedback extends React.Component {
   constructor(props) {
@@ -21,18 +22,32 @@ class Feedback extends React.Component {
     this.setState({ photo: hash });
   }
 
+  renderMessage() {
+    const getAssertions = JSON.parse(localStorage.getItem('state'));
+    const { player: { assertions } } = getAssertions;
+    const lowScore = 3;
+    if (assertions < lowScore) {
+      return <div data-testid="feedback-text">Podia ser melhor...</div>;
+    }
+    return <div data-testid="feedback-text">Mandou bem!</div>;
+  }
+
   render() {
     const { name } = this.props;
     const { photo } = this.state;
-    const score = JSON.parse(localStorage.getItem('state')).player.score;
-    const assertions = JSON.parse(localStorage.getItem('state')).player.assertions;
+    const getScore = JSON.parse(localStorage.getItem('state'));
+    const { player: { score } } = getScore;
     return (
       <div>
         <h1>Resultado</h1>
-        <div data-testid="feedback-text"> { assertions < 3 ? 'Podia ser melhor...' : 'Mandou bem!' }</div>
-        <img src={ `https://www.gravatar.com/avatar/${photo}` } data-testid="header-profile-picture" alt="my profile" />
+        { this.renderMessage() }
+        <img
+          src={ `https://www.gravatar.com/avatar/${photo}` }
+          data-testid="header-profile-picture"
+          alt="my profile"
+        />
         <p data-testid="header-player-name">{ name }</p>
-        <div data-testid="header-score"> { score }</div>
+        <div data-testid="header-score">{ score }</div>
       </div>
     );
   }
@@ -42,5 +57,10 @@ const mapStateToProps = (state) => ({
   email: state.user.email,
   name: state.user.name,
 });
+
+Feedback.propTypes = {
+  email: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 export default connect(mapStateToProps)(Feedback);
