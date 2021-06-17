c816a96cd19e7acba12a7f28adb32d3c25690883
Thiago Kachinsky <tjk091@gmail.com>
2021-06-17 18:13:23 -0300

    Requisito 14

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -6,32 +6,33 @@ import Header from '../components/Header';
 
 class Feedbacks extends React.Component {
   render() {
-    const { assertions, score } = this.props;
+    const { score, assertions } = this.props;
     const THREE = 3;
 
     return (
-      <section>
+      <>
         <Header />
-        <span data-testid="feedback-total-question">{assertions}</span>
-        {' '}
-        <span data-testid="feedback-total-score">{score}</span>
-        <h1 data-testid="feedback-text">
-          {assertions >= THREE ? 'Mandou bem!' : 'Podia ser melhor...'}
-        </h1>
+        <main>
+          <h1 data-testid="feedback-text">
+            {assertions >= THREE ? 'Mandou bem!' : 'Podia ser melhor...'}
+          </h1>
+          <span data-testid="feedback-total-question">{assertions}</span>
+          <span data-testid="feedback-total-score">{score}</span>
+        </main>
         <Link to="/" data-testid="btn-play-again">Jogar novamente</Link>
-      </section>
+      </>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  assertions: state.player.assertions,
   score: state.player.score,
+  assertions: state.player.assertions,
 });
 
-Feedbacks.propTypes = PropTypes.shape({
-  assertions: PropTypes.number,
-  score: PropTypes.number,
-}).isRequired;
+Feedbacks.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+};
 
 export default connect(mapStateToProps)(Feedbacks);
