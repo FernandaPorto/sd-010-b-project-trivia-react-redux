f16f951386dbcdd4e17fa2fe576325d78a74f855
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-06-18 10:28:49 -0300

    resolvido os requisitos 5 e 6

--- src/components/GameTrivia.jsx
+++ src/components/GameTrivia.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-// import { getTriviaApi } from '../actions';
 
 class GameTrivia extends React.Component {
   constructor(props) {
