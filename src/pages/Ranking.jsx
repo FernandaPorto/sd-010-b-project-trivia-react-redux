14eb047ec5205e215a2d1548706853073a14ee3c
Dieghonm <dieghonm@gmail.com>
2021-06-17 23:48:01 -0300

    css  final?

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom';
 class Ranking extends Component {
   render() {
     const getRanking = JSON.parse(localStorage.getItem('ranking'));
-    const gravatar = `https://www.gravatar.com/avatar/${item.emailGravatar}`;
     // const atual = JSON.parse(localStorage.getItem('state'));
     return (
       <section className="question-card">
         <h1 data-testid="ranking-title">RANKING</h1>
         {getRanking.map((item, i) => (
           <div key={ i } className="ranking-item">
-            <img height="20" src={ gravatar } alt="Avatar" />
+            <img
+              height="20"
+              src={ `https://www.gravatar.com/avatar/${item.emailGravatar}` }
+              alt="Avatar"
+            />
             <h3>Name :</h3>
             <h3 data-testid={ `player-name-${i}` }>
               {item.name}
