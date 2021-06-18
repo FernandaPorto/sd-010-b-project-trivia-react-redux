5e37e0245d7a464b873f044362513ef8b0e9fbd6
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-18 12:18:43 -0300

    adicionando a função generateRandomAnswers como função helper

--- src/components/Question.js
+++ src/components/Question.js
@@ -25,23 +25,6 @@ class Question extends Component {
     getQuestions(token);
   }
 
-  generateRandomAnswers(correctAnswer, incorrectAnswer) {
-    const range = 20;
-    const getRandom = () => Math.ceil(Math.random() * range);
-    if (incorrectAnswer.length < 2) {
-      return [
-        { id: getRandom(), answer: correctAnswer, dataTestId: CORRECT_ANSWER },
-        { id: getRandom(), answer: incorrectAnswer, dataTestId: 'wrong-answer-0' },
-      ].sort((a, b) => +(a.id > b.id) || +(a.id === b.id) - 1); // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-    }
-    return [
-      { id: getRandom(), answer: correctAnswer, dataTestId: CORRECT_ANSWER },
-      { id: getRandom(), answer: incorrectAnswer[0], dataTestId: 'wrong-answer-0' },
-      { id: getRandom(), answer: incorrectAnswer[1], dataTestId: 'wrong-answer-1' },
-      { id: getRandom(), answer: incorrectAnswer[2], dataTestId: 'wrong-answer-2' },
-    ].sort((a, b) => +(a.id > b.id) || +(a.id === b.id) - 1); // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-  }
-
   insertClass() {
     const correctButton = document.querySelector('button[data-testid="correct-answer"]');
     const wrongButton = document.querySelectorAll('button[data-testid*="wrong-answer"]');
