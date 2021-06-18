acfe4b2854dc7c31388cce56cfb4b4aafe06159c
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-06-18 15:15:42 -0300

    Feat: requirement 16 -DONE

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+
 import HeaderFeedback from '../components/HeaderFeedback';
 
 class Feedback extends React.Component {
@@ -18,7 +17,7 @@ class Feedback extends React.Component {
     }
     return (<p data-testid="feedback-text">Mandou bem!</p>);
   }
-  
+
   render() {
     return (
       <main>
@@ -35,6 +34,9 @@ class Feedback extends React.Component {
           </div>
           <button type="button" data-testid="btn-play-again">
             <Link to="/">Jogar novamente</Link>
+          </button>
+          <button type="button" data-testid="btn-ranking">
+            <Link to="/ranking">Ranking</Link>
           </button>
         </section>
       </main>
