9d8abd72caabed699ac3c23f86e5518f853d336b
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-18 12:35:31 -0300

    o atributo name renderizado apartir do localStorage

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
 import Question from '../components/Question';
-import Cronometer from '../components/Cronometer';
+import { getPlayerDataLocalStorage } from '../helpers/localStorage';
 
 class GamePage extends React.Component {
   render() {
-    const { name, score } = this.props;
+    const { score } = this.props;
+    const name = getPlayerDataLocalStorage('name');
     return (
       <>
         <header>
@@ -21,13 +22,11 @@ class GamePage extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-  name: state.player.name,
   score: state.player.score,
 });
 
 export default connect(mapStateToProps)(GamePage);
 
 GamePage.propTypes = {
-  name: propTypes.string.isRequired,
   score: propTypes.number.isRequired,
 };
