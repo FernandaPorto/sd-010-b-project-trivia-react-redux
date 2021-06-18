f53f982ce3d3ce8c441dd0f3db35deb6b6403f94
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-06-18 19:10:08 -0300

    botao de voltar a tela inicial

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Ranking extends Component {
   render() {
     return (
       <section>
         <h1 data-testid="ranking-title"> Ranking </h1>
+
+        <Link to="/">
+          <button data-testid="btn-go-home" type="button">
+            Voltar para Home
+          </button>
+        </Link>
       </section>
     );
   }
