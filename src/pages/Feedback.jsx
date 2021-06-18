dcd76ec8043c90fad554e4e78617c900273c9499
Matheus Bodra <matheusbodra@gmail.com>
2021-06-18 20:35:36 -0300

    requisito 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+const THREE_POINTS = 3;
+
 class Feedback extends React.Component {
   render() {
-    const { gravatarEmail, name, score } = this.props;
+    const { gravatarEmail, name, score, assertions } = this.props;
+
     return (
       <main>
         <h2 data-testid="feedback-text">
-          Mensagem
+          { assertions >= THREE_POINTS ? 'Mandou bem!' : 'Podia ser melhor...' }
         </h2>
         <img src={ gravatarEmail } alt={ name } data-testid="header-profile-picture" />
         <h3 data-testid="header-player-name">{ name }</h3>
@@ -19,11 +22,12 @@ class Feedback extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  const { player: { gravatarEmail, name, score } } = state;
+  const { player: { gravatarEmail, name, score, assertions } } = state;
   return {
     name,
     score,
     gravatarEmail,
+    assertions,
   };
 };
 
@@ -31,6 +35,7 @@ Feedback.propTypes = {
   name: PropTypes.string.isRequired,
   gravatarEmail: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
