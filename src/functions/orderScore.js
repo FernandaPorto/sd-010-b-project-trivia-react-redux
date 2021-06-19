8fa047abef7746673d0f52aa6cb0f9b6fc16aa12
brunobastosfer <brunobastos@edu.unifor.br>
2021-06-19 11:13:03 -0300

    ajust orderscore

--- src/functions/orderScore.js
+++ src/functions/orderScore.js
@@ -3,11 +3,12 @@ function orderScore() {
   const objectInfos = JSON.parse(InfoLocalStorage);
   // Função responsável para ordernar o array score
   const onlyScore = objectInfos.map((item) => item.player.score);
+  const orderScore = onlyScore.sort((a, b) => b - a);
   // Lógica inválida pois ordena o score e aplica ao primeiro player(e se ele não for o que tirou máxima pontuação??)
   const newObjectInfos = objectInfos.map((item, index) => ({
     gravatarEmail: item.player.gravatarEmail,
     name: item.player.name,
-    score: onlyScore[index],
+    score: orderScore[index],
   }));
   return newObjectInfos;
 }
