ddbdd951df4c593ae5ca490d5ebaafc496bf3416
brunobastosfer <brunobastos@edu.unifor.br>
2021-06-19 11:13:37 -0300

    renomeiando uma variavel

--- src/functions/orderScore.js
+++ src/functions/orderScore.js
@@ -3,12 +3,12 @@ function orderScore() {
   const objectInfos = JSON.parse(InfoLocalStorage);
   // Função responsável para ordernar o array score
   const onlyScore = objectInfos.map((item) => item.player.score);
-  const orderScore = onlyScore.sort((a, b) => b - a);
+  const decrescentScore = onlyScore.sort((a, b) => b - a);
   // Lógica inválida pois ordena o score e aplica ao primeiro player(e se ele não for o que tirou máxima pontuação??)
   const newObjectInfos = objectInfos.map((item, index) => ({
     gravatarEmail: item.player.gravatarEmail,
     name: item.player.name,
-    score: orderScore[index],
+    score: decrescentScore[index],
   }));
   return newObjectInfos;
 }
