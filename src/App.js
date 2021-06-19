9e7b036c4ef3d1c2a9f1cb300933eafb2c093153
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-19 01:00:06 -0300

    adicionando o componente mockFeedback para aprovar o requisito 11

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 // import logo from './trivia.png';
 import './App.css';
+import MockFeedback from './pages/MockFeedback';
 import GamePage from './pages/GamePage';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
@@ -15,6 +16,7 @@ export default function App() {
           <Route exact path="/" component={ Login } />
           <Route exact path="/setting" component={ Settings } />
           <Route exact path="/game" component={ GamePage } />
+          <Route exact path="/feedback" component={ MockFeedback } />
         </Switch>
       </header>
     </div>
