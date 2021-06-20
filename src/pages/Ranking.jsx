2415106b5b429993b29281fbbc3ca5aee1db7bcd
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-06-20 12:35:55 -0300

    css: Finalizando ranking

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
-import { getFromLocalStorage } from '../services/helpers/localStorage';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import triviaBigodesLogo from "../assets/images/headerBigodes.png";
+import { getFromLocalStorage } from "../services/helpers/localStorage";
 
 class Ranking extends Component {
   constructor(props) {
@@ -10,35 +10,41 @@ class Ranking extends Component {
   }
 
   handlesort() {
-    const scoreRanking = getFromLocalStorage('ranking');
-    return scoreRanking
-      .sort((a, b) => (b.score - a.score));
+    const scoreRanking = getFromLocalStorage("ranking");
+    return scoreRanking.sort((a, b) => b.score - a.score);
   }
 
   render() {
     return (
-      <div className="bg-light_gray_color">
-        <section data-testid="ranking-title">
-          Titulo
-        </section>
-        <Link to="/">
-          <button type="button" data-testid="btn-go-home">
-            Inicio
-          </button>
-        </Link>
-        <ul className="flex justify-center items-center w-screen h-screen flex-col">
-          {this.handlesort().map((element, index) => {
-            const { name, score, picture } = element;
-            return (
-              <li key={ index } className="flex bg-primary_color justify-around py-5">
-                <img src={ picture } alt={ name } />
-                {' '}
-                <span data-testid={ `player-score-${index}` } className="flex justify-around">{score}</span>
-                {' '}
-                <span data-testid={ `player-name-${index}` }>{name}</span>
-              </li>);
-          })}
-        </ul>
+      <div className="bg-light_gray_color flex flex-col justify-center items-center ">
+        <div className="w-full">
+          <Link to="/">
+            <button type="button" data-testid="btn-go-home">
+              Home
+            </button>
+          </Link>
+        </div>
+        <div
+          className="flex flex-col items-center bg-primary_color
+          shadow-lg min-w-1/4 min-h-1/4 max-h-3/4 rounded-lg"
+        >
+          <img className="w-40" src={triviaBigodesLogo} alt="Logo trivia Bigodes" />
+          <section data-testid="ranking-title">Ranking</section>
+          <ul className="">
+            {this.handlesort().map((element, index) => {
+              const { name, score, picture } = element;
+              return (
+                <li key={index} className="">
+                  <img src={picture} alt={name} />{" "}
+                  <span data-testid={`player-score-${index}`} className="flex justify-around">
+                    {score}
+                  </span>{" "}
+                  <span data-testid={`player-name-${index}`}>{name}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     );
   }
