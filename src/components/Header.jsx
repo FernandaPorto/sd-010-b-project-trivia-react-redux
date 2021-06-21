c0fa7b79f8b231e74c24d6359b9beeeeb8f9af7f
Pedro Toselli <pedrotrybe@gmail.com>
2021-06-21 16:15:23 -0300

    Implementing Header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import './css/Header.css';
 
 class Header extends Component {
   render() {
     const { name, score, picture } = this.props;
 
     return (
-      <header>
-        <img src={ picture } alt="avatar" data-testid="header-profile-picture" />
-        <p data-testid="header-player-name">{ name }</p>
-        <p data-testid="header-score">{ score }</p>
+      <header id="header">
+        <img
+          id="avatar-img"
+          src={ picture }
+          alt="avatar"
+          data-testid="header-profile-picture"
+        />
+        <p id="avatar-name" data-testid="header-player-name">{ name }</p>
+        <p id="avatar-score" data-testid="header-score">{ score }</p>
       </header>
     );
   }
